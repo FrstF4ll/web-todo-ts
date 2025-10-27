69e41ff3a8c7ef0e46230b22a5a907a17124acec
FrstF4ll <hydralixe@gmail.com>
2025-10-27 14:49:26 +0100

    feat(main.ts): add whitespace-check

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import './style.css'
 
 // Get elements from HTML
-const toDoInput = document.querySelector<HTMLInputElement>('#todo-input')!;
-const addButton = document.querySelector<HTMLButtonElement>('#add-todo-button')!;
-const toDoList = document.querySelector<HTMLUListElement>('ul')!;
+const toDoInput = document.querySelector<HTMLInputElement>('#todo-input')!
+const addButton = document.querySelector<HTMLButtonElement>('#add-todo-button')!
+const toDoList = document.querySelector<HTMLUListElement>('ul')!
 
 function addToList(e: KeyboardEvent): any {
   if (e.key === 'Enter') {
-    toDoInput.value 
-    ? toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
-    : console.log('Invalid input') 
+    const regex = /^\s*$/ 
+    !toDoInput.value || regex.test(toDoInput.value)  
+    ? console.log("Invalid input")
+    :    toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
+    }
   }
-}
 
 toDoInput.addEventListener('keydown', addToList)
 addButton.addEventListener('keydown', addToList)
