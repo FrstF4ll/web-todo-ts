d9051357f1d4a947f9edd5925db4d6d16a8907dc
FrstF4ll <hydralixe@gmail.com>
2025-10-27 14:16:28 +0100

    chore(main.ts): store DOM element reference in const via querySelector

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import './style.css'
 
-console.log('Hello from typescript')
+// Get elements from HTML
+const toDoInput = document.querySelector<HTMLInputElement>('#todo-input')!
+const addButton = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const toDoList = document.querySelector<HTMLUListElement>('ul')!
+
+
