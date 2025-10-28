1de2f26f99ecbbfbe8f331049e5faa547c1e2253
FrstF4ll <hydralixe@gmail.com>
2025-10-28 08:21:12 +0100

    fix: add placeholder, label for input and fix error message not displaying correctly

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ const addButton = document.querySelector<HTMLButtonElement>('#add-todo-button')!
 const toDoList = document.querySelector<HTMLUListElement>('ul')!
 
 const errorMsg = document.createElement('p')
-errorMsg.innerText = 'Error'
+errorMsg.style.display = 'none'
 toDoInput.parentNode!.insertBefore(errorMsg, toDoInput)
 
 function addToList(e: KeyboardEvent): void {
   if (e.key === 'Enter') {
     const regex = /^\s*$/ // Match 0 or more whitespace
     if (!toDoInput.value || regex.test(toDoInput.value)) {
-      console.log('error')
+      errorMsg.style.display = 'block'
+      errorMsg.innerText = 'Error'
     } else {
+      errorMsg.style.display = 'none'
       toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
     }
   }
@@ -23,8 +25,10 @@ function addToList(e: KeyboardEvent): void {
 function addToListByClick(): void {
   const regex = /^\s*$/ // Match 0 or more whitespace
   if (!toDoInput.value || regex.test(toDoInput.value)) {
-    console.log('error')
+errorMsg.style.display = 'block'
+    errorMsg.innerText = 'Error'
   } else {
+    errorMsg.style.display = 'none'
     toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
   }
 }
