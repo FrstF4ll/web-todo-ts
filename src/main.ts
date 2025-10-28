a21b92ba1d9bb1baeb64e150a8f93ab1ef6a91a6
FrstF4ll <hydralixe@gmail.com>
2025-10-28 07:43:41 +0100

    feat(main.ts): add event listener on 'click'

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ const toDoInput = document.querySelector<HTMLInputElement>('#todo-input')!
 const addButton = document.querySelector<HTMLButtonElement>('#add-todo-button')!
 const toDoList = document.querySelector<HTMLUListElement>('ul')!
 
-function addToList(e: KeyboardEvent): any {
-  if (e.key === 'Enter') {
-    const regex = /^\s*$/ 
-    if(!toDoInput.value || regex.test(toDoInput.value)){
-    console.log("Invalid input")
-    return
-    }
-    else {
-        toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
+const errorMsg = document.createElement('p')
+errorMsg.innerText = 'Error'
+toDoInput.parentNode!.insertBefore(errorMsg, toDoInput)
+
+function addToList(e: KeyboardEvent): void {
+  if (e.key === 'Enter' || e.button === 'click') { 
+    const regex = /^\s*$/ // Match 0 or more whitespace
+    if (!toDoInput.value || regex.test(toDoInput.value)) {
+    } else {
+      toDoList.innerHTML += `<li id="todo-elements">${toDoInput.value}</li>`
     }
   }
 }
-toDoInput.addEventListener('keydown', addToList)
+
+toDoInput.addEventListener('click', addToList)
 addButton.addEventListener('keydown', addToList)
