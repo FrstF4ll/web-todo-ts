3b7a8280a25fcdc4a117c722d4777f42ff1e2a94
FrstF4ll <hydralixe@gmail.com>
2025-11-03 13:13:00 +0100

    fix(addToList/renderTask): fixed invisible tasks
    - Task were previously not displaying properly after separating both functions.

--- src/main.ts
+++ src/main.ts
@@ -13,31 +13,31 @@ const addButton = getRequiredElement<HTMLButtonElement>('#add-todo-button')
 const toDoList = getRequiredElement<HTMLUListElement>('ul')
 const errorMsg = getRequiredElement<HTMLParagraphElement>('#error-msg')
 const taskList: string[] = JSON.parse(localStorage.getItem('tasks') || '[]')
-const taskText = toDoInput.value.trim()
 
 //Rendering
-function renderTask(): void{
+function renderTask(taskText: string): void {
   const newTask = document.createElement('li')
   newTask.className = 'todo-elements'
   newTask.textContent = taskText
   toDoList.appendChild(newTask)
-  toDoInput.value = '' 
-    taskList.push(taskText);  
-  localStorage.setItem('tasks', JSON.stringify(taskList)); 
+  toDoInput.value = ''
+
+  // Storage update
+  taskList.push(taskText)
+  localStorage.setItem('tasks', JSON.stringify(taskList))
 }
 
-//Validation 
+//Validation
 function addToList(): void {
+  const taskText = toDoInput.value.trim()
   if (!taskText) {
     errorMsg.classList.remove('hidden')
     return
   }
   errorMsg.classList.add('hidden')
-  renderTask()
+  renderTask(taskText)
   return
 }
-
-
 
 toDoInput.addEventListener('keydown', (e: KeyboardEvent) => {
   if (e.key === 'Enter') {
