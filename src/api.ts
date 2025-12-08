fb71e89a3ce3be5503d8ca3b5f2605806a5f39eb
FrstF4ll <hydralixe@gmail.com>
2025-12-08 15:10:28 +0100

    fix(post): handle duplication of data on post.
    - Also store the value of the first object in the array in a var

--- src/api.ts
+++ src/api.ts
@@ -52,7 +52,15 @@ export async function postData<T>(
 
     await handleApiError(response)
     const responseArr = (await response.json()) as T[]
-    return responseArr[0]
+    if (responseArr.length > 1) {
+      console.warn(
+        'POST-ERROR: Expected one object but got two. Check for duplication.',
+        responseArr,
+      )
+      return responseArr as T
+    }
+    const singleObject = responseArr[0]
+    return singleObject as T
   } catch (error) {
     console.error(`Data failed to post to ${apiURL}: `, error)
     throw error
