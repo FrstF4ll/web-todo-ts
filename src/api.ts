74ca7ce10028b41b3d45099d0e011d18009cf02f
FrstF4ll <hydralixe@gmail.com>
2025-12-08 09:58:55 +0100

    refactor: move API functions to dedicated file

---
+++ src/api.ts
@@ -0,0 +1,125 @@
+import type { ClientTask } from './interface'
+
+//API error handling
+async function handleApiError(response: Response): Promise<void> {
+  if (!response.ok) {
+    let errorDetails = `HTTP Error ${response.status}: ${response.statusText}.`
+    try {
+      const errorBody = await response.json()
+      errorDetails += ` Server Message: ${JSON.stringify(errorBody)}`
+    } catch (parseError) {
+      console.error(`Query failed : ${parseError}`)
+    }
+    throw new Error(errorDetails)
+  }
+}
+
+//Get API data
+export async function getData<T>(apiURL: string): Promise<T[]> {
+  try {
+    const response = await fetch(apiURL, {
+      method: 'GET',
+    })
+
+    await handleApiError(response)
+
+    if (response.status === 204) {
+      return [] as T[]
+    }
+    const fetchedData = await response.json()
+    if (Array.isArray(fetchedData)) {
+      return fetchedData as T[]
+    }
+    console.warn(`API at ${apiURL} returned data, but it was not an array`)
+    return [] as T[]
+  } catch (error) {
+    console.error('Failed to fetch data', error)
+    throw error
+  }
+}
+
+//Post request
+export async function postData<T>(
+  apiURL: string,
+  newDatas: ClientTask,
+): Promise<T | null> {
+  try {
+    const response = await fetch(apiURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newDatas),
+    })
+    await handleApiError(response)
+
+    if (response.status === 204) {
+      return null
+    }
+    const text = await response.text()
+    if (!text) {
+      return null
+    }
+    const createdData: unknown = JSON.parse(text)
+    return createdData as T
+  } catch (error) {
+    console.error(`Data failed to post to ${apiURL}: `, error)
+    throw error
+  }
+}
+
+//Patch request
+export async function patchData<C, R>(
+  apiURL: string,
+  id: number,
+  updatedDatas: C,
+): Promise<R | null> {
+  const completeURL = `${apiURL}?id=eq.${id}`
+  try {
+    const response = await fetch(completeURL, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedDatas),
+    })
+    await handleApiError(response)
+    if (response.status === 204) {
+      return null as R
+    }
+
+    const updatedResource: unknown = await response.json()
+    return updatedResource as R
+  } catch (error) {
+    console.error(`Patch failed for task ${id} at ${completeURL}:`, error)
+    throw error
+  }
+}
+
+//Delete request
+export async function deleteData(apiURL: string, id: number): Promise<void> {
+  const completeURL = `${apiURL}?id=eq.${id}`
+  try {
+    const response = await fetch(completeURL, {
+      method: 'DELETE',
+    })
+    await handleApiError(response)
+  } catch (error) {
+    console.error(`Delete failed for task ${id} at ${completeURL}:`, error)
+    throw error
+  }
+}
+
+// Delete request
+export async function deleteAllData(apiURL: string): Promise<void> {
+  const completeURL = `${apiURL}`
+  try {
+    const response = await fetch(completeURL, {
+      method: 'DELETE',
+    })
+    await handleApiError(response)
+  } catch (error) {
+    console.error(`Delete failed at ${completeURL}:`, error)
+    throw error
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style.css'
+
+import { deleteAllData, deleteData, getData, patchData, postData } from './api'
 // DOM import
 import {
   addButton,
@@ -13,134 +15,10 @@ import {
 import type { ClientTask, Task } from './interface'
 // Time calculation
 import { dateColorSetUp, toMidnight } from './utils'
-
 // API endpoints
 export const API_URL_TODOS: string = 'https://api.todos.in.jt-lab.ch/todos'
 // const CATEGORIES_API_ENDPOINT: string = 'https://api.todos.in.jt-lab.ch/categories'
 // const CATEGORIES_TODO_API_ENDPOINT: string = 'https://api.todos.in.jt-lab.ch/categories_todos'
-
-//API error handling
-async function handleApiError(response: Response): Promise<void> {
-  if (!response.ok) {
-    let errorDetails = `HTTP Error ${response.status}: ${response.statusText}.`
-    try {
-      const errorBody = await response.json()
-      errorDetails += ` Server Message: ${JSON.stringify(errorBody)}`
-    } catch (parseError) {
-      console.error(`Query failed : ${parseError}`)
-    }
-    throw new Error(errorDetails)
-  }
-}
-
-//Get API data
-async function getData<T>(apiURL: string): Promise<T[]> {
-  try {
-    const response = await fetch(apiURL, {
-      method: 'GET',
-    })
-
-    await handleApiError(response)
-
-    if (response.status === 204) {
-      return [] as T[]
-    }
-    const fetchedData = await response.json()
-    if (Array.isArray(fetchedData)) {
-      return fetchedData as T[]
-    }
-    console.warn(`API at ${apiURL} returned data, but it was not an array`)
-    return [] as T[]
-  } catch (error) {
-    console.error('Failed to fetch data', error)
-    throw error
-  }
-}
-
-//Post request
-async function postData<T>(
-  apiURL: string,
-  newDatas: ClientTask,
-): Promise<T | null> {
-  try {
-    const response = await fetch(apiURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newDatas),
-    })
-    await handleApiError(response)
-
-    if (response.status === 204) {
-      return null
-    }
-    const text = await response.text()
-    if (!text) {
-      return null
-    }
-    const createdData: unknown = JSON.parse(text)
-    return createdData as T
-  } catch (error) {
-    console.error(`Data failed to post to ${apiURL}: `, error)
-    throw error
-  }
-}
-
-//Patch request
-export async function patchData<C, R>(
-  apiURL: string,
-  id: number,
-  updatedDatas: C,
-): Promise<R | null> {
-  const completeURL = `${apiURL}?id=eq.${id}`
-  try {
-    const response = await fetch(completeURL, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedDatas),
-    })
-    await handleApiError(response)
-    if (response.status === 204) {
-      return null as R
-    }
-
-    const updatedResource: unknown = await response.json()
-    return updatedResource as R
-  } catch (error) {
-    console.error(`Patch failed for task ${id} at ${completeURL}:`, error)
-    throw error
-  }
-}
-
-//Delete request
-async function deleteData(apiURL: string, id: number): Promise<void> {
-  const completeURL = `${apiURL}?id=eq.${id}`
-  try {
-    const response = await fetch(completeURL, {
-      method: 'DELETE',
-    })
-    await handleApiError(response)
-  } catch (error) {
-    console.error(`Delete failed for task ${id} at ${completeURL}:`, error)
-    throw error
-  }
-}
-// Delete request
-async function deleteAllData(apiURL: string): Promise<void> {
-  const completeURL = `${apiURL}`
-  try {
-    const response = await fetch(completeURL, {
-      method: 'DELETE',
-    })
-    await handleApiError(response)
-  } catch (error) {
-    console.error(`Delete failed at ${completeURL}:`, error)
-    throw error
-  }
-}
 
 // Loading tasks
 try {
