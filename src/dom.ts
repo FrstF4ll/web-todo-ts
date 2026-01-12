0b470b9afe3c7adc5b8c8a01cd02d7ab10fa259f
FrstF4ll <hydralixe@gmail.com>
2026-01-12 08:24:03 +0100

    feat: implement global constants for dom managment

--- src/dom.ts
+++ src/dom.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import { SEL } from './constants'
 
 // Get only non-null element.
 function getRequiredElement<T extends HTMLElement>(selector: string): T {
@@ -6,17 +7,20 @@ function getRequiredElement<T extends HTMLElement>(selector: string): T {
   if (!el) throw new Error(`Element ${selector} not found`)
   return el
 }
+
+
+
 // DOM
-export const toDoInput = getRequiredElement<HTMLInputElement>('#todo-input')
+export const toDoInput = getRequiredElement<HTMLInputElement>(SEL.TODO_INPUT)
 export const addButton =
-  getRequiredElement<HTMLButtonElement>('#add-todo-button')
-export const toDoList = getRequiredElement<HTMLUListElement>('ul')
-export const errorMsg = getRequiredElement<HTMLParagraphElement>('#error-msg')
-export const clearAllBtn = getRequiredElement<HTMLButtonElement>('#delete-all')
+  getRequiredElement<HTMLButtonElement>(SEL.ADD_BUTTON)
+export const toDoList = getRequiredElement<HTMLUListElement>(SEL.TODO_LIST)
+export const errorMsg = getRequiredElement<HTMLParagraphElement>(SEL.ERROR_MESSAGE)
+export const clearAllBtn = getRequiredElement<HTMLButtonElement>(SEL.DELETE_ALL)
 export const dateInput =
-  getRequiredElement<HTMLInputElement>('#todo-date-input')
+  getRequiredElement<HTMLInputElement>(SEL.DATE_INPUT)
 export const overdueMsg =
-  getRequiredElement<HTMLHeadingElement>('#overdue-message')
+  getRequiredElement<HTMLHeadingElement>(SEL.OVERDUE_MESSAGE)
 // Show or hide error message
 export const showStatusMessage = (message: string) => {
   errorMsg.classList.remove('hidden')
