51cad53a1cc79e81a450bfab971ae6c1bde39133
FrstF4ll <hydralixe@gmail.com>
2026-01-16 15:40:37 +0100

    chore: create type interfaces for categories

--- src/interface.ts
+++ src/interface.ts
@@ -9,3 +9,12 @@ export interface ClientTask {
 export interface Task extends ClientTask {
   id: number
 }
+
+export interface ClientCategory {
+  title: string
+  color: string
+}
+
+export interface Category extends ClientCategory {
+  id: number
+}
